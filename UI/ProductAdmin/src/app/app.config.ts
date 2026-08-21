27ed9f0badbd414a7f3fd17a543d57506165a92e
Bill Barge <wbarge@cox.net>
2026-08-20 17:23:26 -0700

    update angular

--- UI/ProductAdmin/src/app/app.config.ts
+++ UI/ProductAdmin/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig,importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { routes } from './app.routes';
 import { MessageService } from 'primeng/api';
 
